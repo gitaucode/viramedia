f75dbddafa43b3a79a2513247f3d5a3fd3b91386
Gitau.Code <gitau.code@gmail.com>
2026-08-20 14:10:37 +0300

    Add client portal layout

---
+++ src/app/client/layout.tsx
@@ -0,0 +1,4 @@
+import type { Metadata } from "next";
+import "./client.css";
+export const metadata:Metadata={title:"Client Portal | Vira Media",robots:{index:false,follow:false}};
+export default function ClientLayout({children}:{children:React.ReactNode}){return children}
