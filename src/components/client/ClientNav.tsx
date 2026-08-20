d7d7ffb37d122242f1aa6a1aae12bf619bf59030
Gitau.Code <gitau.code@gmail.com>
2026-08-20 14:10:04 +0300

    Add client portal navigation

---
+++ src/components/client/ClientNav.tsx
@@ -0,0 +1,2 @@
+"use client";
+export default function ClientNav(){async function logout(){await fetch('/api/client/logout',{method:'POST'});location.href='/client/login'}return <nav className="client-nav"><div className="client-nav-inner"><a className="client-brand" href="/client/dashboard">VIRA <span>CLIENT PORTAL</span></a><div className="client-nav-actions"><a href="/client/dashboard">Campaigns</a><button onClick={()=>void logout()}>Log out</button></div></div></nav>}
