42260ce6fe0844ad966041ac87635959d8f72be7
Gitau.Code <gitau.code@gmail.com>
2026-08-20 11:02:31 +0300

    Add creator portal layout

---
+++ src/app/portal/layout.tsx
@@ -0,0 +1,4 @@
+import type { Metadata } from "next";
+import "./portal.css";
+export const metadata:Metadata={title:"Creator Portal | Vira Network",robots:{index:false,follow:false}};
+export default function PortalLayout({children}:{children:React.ReactNode}){return children}
